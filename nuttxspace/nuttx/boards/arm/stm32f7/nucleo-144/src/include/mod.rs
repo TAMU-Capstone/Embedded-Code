65aa6c3f7fb89ad0fb20e212853591fff2e07735
Joshua <joshuamclapp@gmail.com>
2024-03-06 12:15:45 -0600

    small changes, need to find files

--- nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/include/mod.rs
+++ nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/include/mod.rs
@@ -13,3 +13,7 @@ pub const GPIO_LD3: u32 = GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | GPIO_
 pub const GPIO_LED_GREEN: u32 = GPIO_LD1;
 pub const GPIO_LED_BLUE: u32 = GPIO_LD2;
 pub const GPIO_LED_RED: u32 = GPIO_LD3;
+
+
+// used by boot.c
+pub const CONFIG_STM32F7_OTGFS: 
