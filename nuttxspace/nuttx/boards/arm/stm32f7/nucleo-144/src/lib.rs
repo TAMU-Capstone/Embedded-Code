522ac6eef0979b49d5e28d2af5e07e5346c48cb0
svettlira <svettlira@gmail.com>
2024-03-27 13:09:49 -0500

    moved initialized varibale in main function of adc outside cfg to fix not found error

--- nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/lib.rs
+++ nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/lib.rs
@@ -14,11 +14,5 @@ mod bindings;
 mod stm32_autoleds;
 mod stm32_reset;
 mod stm32_appinitialize;
-<<<<<<< HEAD
 mod stm32_adc;
 mod stm32_gpio;
-=======
-//mod stm32_adc;
-mod stm32_buttons;
-mod stm32_userleds;
->>>>>>> d4b78a6dc54190914a80fdb30beb828d7c456051
