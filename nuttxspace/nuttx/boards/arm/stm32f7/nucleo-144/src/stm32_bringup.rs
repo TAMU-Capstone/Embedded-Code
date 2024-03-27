819e95f8cc5bd8eb45e06aea3b93e4869992d04d
isidorawright <isidora.wright.1@gmail.com>
2024-03-27 12:01:52 -0500

    getting up to date

--- nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/stm32_bringup.rs
+++ nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/stm32_bringup.rs
@@ -71,14 +71,7 @@ use crate::bindings::*;
         ret = nx_mount(null_ptr, STM32_PROCFS_MOUNTPOINT, "procfs", 0, null_ptr);
         if ret < 0
         {
-<<<<<<< HEAD
-            unsafe{
-                syslog(LOG_ERR, "ERROR: Failed to mount procfs at %s: %d\n",
-                STM32_PROCFS_MOUNTPOINT, ret);
-            }
-=======
             pub fn syslog(LOG_ERR: u8, "ERROR: Failed to mount procfs at %s: %d\n": &str, STM32_PROCFS_MOUNTPOINT, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
         }
     }
 
@@ -90,13 +83,9 @@ use crate::bindings::*;
 
         if ret < 0
         {
-<<<<<<< HEAD
             unsafe{
                 syslog(LOG_ERR, "ERROR: Failed to mount romfs at %s: %d\n", CONFIG_STM32_ROMFS_MOUNTPOINT, ret);
             }
-=======
-            pub fn syslog(LOG_ERR: u8, "ERROR: Failed to mount romfs at %s: %d\n": &str, CONFIG_STM32_ROMFS_MOUNTPOINT, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
         }
     }
 
@@ -106,13 +95,9 @@ use crate::bindings::*;
         ret = stm32_gpio_initialize();
         if ret < 0
         {
-<<<<<<< HEAD
             unsafe{
                 syslog(LOG_ERR, "Failed to initialize GPIO Driver: %d\n", ret);
             }
-=======
-            pub fn syslog(LOG_ERR: u8, "Failed to initialize GPIO Driver: %d\n": &str, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
             return ret;
         }
     }
@@ -122,13 +107,9 @@ use crate::bindings::*;
         ret = userled_lower_initialize(LED_DRIVER_PATH);
         if ret < 0
         {
-<<<<<<< HEAD
             unsafe{
                 syslog(LOG_ERR, "ERROR: userled_lower_initialize() failed: {}\n", ret);
             }
-=======
-            pub fn syslog(LOG_ERR: u8, "ERROR: userled_lower_initialize() failed: {}\n": &str, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
         }
     }
 
@@ -137,13 +118,9 @@ use crate::bindings::*;
         ret = stm32_adc_setup();
         if ret < 0
         {
-<<<<<<< HEAD
             unsafe{
                 syslog(LOG_ERR, "ERROR: stm32_adc_setup failed: %d\n", ret);
             }
-=======
-            pub fn syslog(LOG_ERR: u8, "ERROR: stm32_adc_setup failed: %d\n": &str, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
         }
     }
 
@@ -157,18 +134,11 @@ use crate::bindings::*;
     if cfg!(CONFIG_FAT_DMAMEMORY)
     {
         //TO-DO: may need to use let and make variable and then compare
-<<<<<<< HEAD
         unsafe{
             if stm32_dma_alloc_init() < 0
             {
                 syslog(LOG_ERR, "DMA alloc FAILED");
             }
-=======
-        let temp = stm32_dma_alloc_init();
-        if temp < 0
-        {
-            pub fn syslog(LOG_ERR: u8, "DMA alloc FAILED": &str);
->>>>>>> 774549ed (fixing merge conflicts)
         }
     }
 
@@ -178,11 +148,7 @@ use crate::bindings::*;
         //there is if ret != OK
         if ret != OK
         {
-<<<<<<< HEAD
             syslog(LOG_ERR, "ERROR: Failed to initialize SPI interfaces: %d\n", ret);
-=======
-            pub fn syslog(LOG_ERR: u8, "ERROR: Failed to initialize SPI interfaces: %d\n": &str, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
             return ret;
         }
     }
@@ -192,11 +158,7 @@ use crate::bindings::*;
         ret = stm32_sdio_initialize();
         if ret != OK
         {
-<<<<<<< HEAD
             ferr("ERROR: Failed to initialize MMC/SD driver: %d\n", ret);
-=======
-            pub fn ferr("ERROR: Failed to initialize MMC/SD driver: %d\n": &str, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
             return ret;
         }
     }
@@ -206,18 +168,13 @@ use crate::bindings::*;
         ret = stm32_pwm_setup();
         if ret < 0
         {
-<<<<<<< HEAD
             syslog(LOG_ERR, "ERROR: stm32_pwm_setup() failed: %d\n", ret);
-=======
-            pub fn syslog(LOG_ERR: u8, "ERROR: stm32_pwm_setup() failed: %d\n": &str, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
         }
     }
 
     if cfg!(CONFIG_SENSORS_QENCODER)
     {
         //defines an array of size 9 and initializes it to 0
-<<<<<<< HEAD
         let mut buf : [i32; 9] = [0; 9];
         
         if cfg!(CONFIG_STM32F7_TIM1_QE)
@@ -231,25 +188,12 @@ use crate::bindings::*;
             unsafe{
                 syslog(LOG_ERR, "ERROR: Failed to register the qencoder: %d\n", ret);
             }
-=======
-        let mut buf: [i32; 9] = [0; 9];
-    }
-    
-    if cfg!(CONFIG_STM32F7_TIM1_QE)
-    {
-        pub fn snprintf(buf: &[i32], buf.len(): i32, "/dev/qe0": &str);
-        ret = stm32_qencoder_initialize(buf: &[i32], 1);
-        if ret < 0
-        {
-            pub fn syslog(LOG_ERR: u8, "ERROR: Failed to register the qencoder: %d\n": &str, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
             return ret;
         }
     }
     
     if cfg!(CONFIG_STM32F7_TIM3_QE)
     {
-<<<<<<< HEAD
         unsafe{
             snprintf(buf, buf.len(), "/dev/qe2");
         }
@@ -261,20 +205,12 @@ use crate::bindings::*;
             unsafe{
                 syslog(LOG_ERR, "ERROR: Failed to register the qencoder: %d\n", ret);
             }
-=======
-        pub fn snprintf(buf: &[i32], sizeof(buf): i32, "/dev/qe2": &str);
-        ret = stm32_qencoder_initialize(buf: &[i32], 3);
-        if ret < 0
-        {
-            pub fn syslog(LOG_ERR: u8, "ERROR: Failed to register the qencoder: %d\n": &str, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
             return ret;
         }
     }
 
     if cfg!(CONFIG_STM32F7_TIM4_QE)
     {
-<<<<<<< HEAD
         unsafe{
             snprintf(buf, buf.len(), "/dev/qe3");
         }
@@ -285,14 +221,7 @@ use crate::bindings::*;
         {
             unsafe{
                 syslog(LOG_ERR, "ERROR: Failed to register the qencoder: %d\n", ret);
-            }
-=======
-        pub fn snprintf(buf: &[i32], buf.len(): i32, "/dev/qe3": &str);
-        ret = stm32_qencoder_initialize(buf: &[i32], 4);
-        if ret < 0
-        {
-            pub fn syslog(LOG_ERR: u8, "ERROR: Failed to register the qencoder: %d\n": &str, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
+            
          return ret;
         }
     }
@@ -303,13 +232,9 @@ use crate::bindings::*;
         ret = stm32_can_setup();
         if ret < 0
         {
-<<<<<<< HEAD
             unsafe{
                 syslog(LOG_ERR, "ERROR: stm32f7_can_setup failed: %d\n", ret);
             }
-=======
-            pub fn syslog(LOG_ERR: u8, "ERROR: stm32f7_can_setup failed: %d\n": &str, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
             return ret;
         }
     }
@@ -321,35 +246,22 @@ use crate::bindings::*;
         }
         if ret < 0
         {
-<<<<<<< HEAD
             unsafe{
                 syslog(LOG_ERR, "ERROR: stm32_cansock_setup failed: %d\n", ret);
             }
-=======
-            pub fn syslog(LOG_ERR: u8, "ERROR: stm32_cansock_setup failed: %d\n": &str, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
         }
     }
 
     if cfg!(CONFIG_I2C) && cfg!(CONFIG_STM32F7_I2C1)
     {
-<<<<<<< HEAD
         let i2c_bus : i32 = 1;
         i2c = stm32_i2cbus_initialize(i2c_bus);
-=======
-        let i2c_bus: i32 = 1;
-        let i2c = stm32_i2cbus_initialize(i2c_bus);
->>>>>>> 774549ed (fixing merge conflicts)
 
         if i2c == null_ptr
         {
-<<<<<<< HEAD
             unsafe{
                 syslog(LOG_ERR, "ERROR: Failed to get I2C%d interface\n", i2c_bus);
             }
-=======
-            pub fn syslog(LOG_ERR: u8, "ERROR: Failed to get I2C%d interface\n": &str, i2c_bus);
->>>>>>> 774549ed (fixing merge conflicts)
         }
         else
         {
@@ -359,44 +271,28 @@ use crate::bindings::*;
             
                 if ret < 0
                 {
-<<<<<<< HEAD
                     unsafe{
                         syslog(LOG_ERR, "ERROR: Failed to register I2C%d driver: %d\n", i2c_bus, ret);
                     }
-=======
-                    pub fn syslog(LOG_ERR: u8, "ERROR: Failed to register I2C%d driver: %d\n": &str, i2c_bus, ret: i32);
->>>>>>> 774549ed (fixing merge conflicts)
                 }
             }
 
             if cfg!(CONFIG_MPU60X0_I2C)
             {
-<<<<<<< HEAD
                 mpu_config = kmm_zalloc( get_size(mpu_config_s ));
                 if mpu_config == None
                 {
                     unsafe{
                         syslog(LOG_ERR, "ERROR: Failed to allocate mpu60x0 driver\n");
                     }
-=======
-                let mpu_config = kmm_zalloc(sizeof(struct mpu_config_s));
-                if mpu_config == null_ptr
-                {
-                    pub fn syslog(LOG_ERR: u8, "ERROR: Failed to allocate mpu60x0 driver\n": &str);
->>>>>>> 774549ed (fixing merge conflicts)
                 }
                 else
                 {
                   mpu_config->i2c = i2c;
                   mpu_config->addr = 0x68;
-<<<<<<< HEAD
                   unsafe{
                     mpu60x0_register("/dev/imu0", mpu_config);
                     }
-
-=======
-                  pub fn mpu60x0_register("/dev/imu0": &str, mpu_config);
->>>>>>> 774549ed (fixing merge conflicts)
                 }
             }
         }
