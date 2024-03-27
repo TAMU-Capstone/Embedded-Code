af39b79cc9ebfd3a207d4c1f6a3907068b9f8324
Cole McAnelly <cole.mcanelly3@gmail.com>
2024-03-27 11:50:53 -0500

    Cleaned up mod.rs to be correct, and merged changes in lib.rs, debugging complex macros in build.rs

--- nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/lib.rs
+++ nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/lib.rs
@@ -11,7 +11,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 mod bindings;
 
-<<<<<<< Updated upstream
 mod stm32_autoleds;
 mod stm32_reset;
 mod stm32_usb;
@@ -21,13 +20,3 @@ mod stm32_bringup;
 mod stm32_appinitialize;
 mod stm32_buttons;
 //pub mod stm32_adc;
-=======
-pub mod stm32_autoleds;
-pub mod stm32_reset;
-pub mod stm32_usb;
-pub mod stm32_boot;
-pub mod stm32_spi;
-pub mod stm32_bringup;
-pub mod stm32_appinitialize;
-pub mod stm32_adc;
->>>>>>> Stashed changes
