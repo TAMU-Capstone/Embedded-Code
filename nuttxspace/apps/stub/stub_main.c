4707b2f4a62c8d66eaf775643e91f6611f798163
Cole McAnelly <cole.mcanelly3@gmail.com>
2024-04-17 13:02:49 -0500

    Added "testingButtons" and renamed "testingApp" -> "testingLeds"

--- nuttxspace/apps/stub/stub_main.c
+++ nuttxspace/apps/stub/stub_main.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/testingApp/testingApp_main.c
+ * apps/examples/hello/hello_main.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -22,109 +22,19 @@
  * Included Files
  ****************************************************************************/
 
-//#include <../../nuttx/include/nuttx/config.h>
 #include <nuttx/config.h>
 #include <stdio.h>
-#include <stdbool.h>
-#include <unistd.h>
-
-
-int GPIO_OUTPUT = 262144;
-int GPIO_PUSHPULL = 0;
-int GPIO_SPEED_50MHz = 2048;
-int OUTPUT_CLEAR = 0;
-int GPIO_PIN0 = 0;
-int GPIO_PIN6 = 6;
-int GPIO_PIN7 = 7;
-int GPIO_PIN14 = 14;
-int GPIO_PORTB = 16;
-
-#define GPIO_LD1       (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | OUTPUT_CLEAR | GPIO_PORTB | GPIO_PIN0)
-#define GPIO_LD2       (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | OUTPUT_CLEAR | GPIO_PORTB | GPIO_PIN7)
-#define GPIO_LD3       (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | OUTPUT_CLEAR | GPIO_PORTB | GPIO_PIN14)
-
-#define GPIO_LED_GREEN GPIO_LD1
-#define GPIO_LED_BLUE  GPIO_LD2
-#define GPIO_LED_RED   GPIO_LD3
-
 
 /****************************************************************************
- * Function Definitions
+ * Public Functions
  ****************************************************************************/
-
-// extern int board_userled_initialize(void);
-// extern void board_userled(int led, _Bool ledon); //Necessary??????????????????????????///
-
 
 /****************************************************************************
- * Test Functions
+ * stub_main
  ****************************************************************************/
 
-void userleds_test(void) {
-  //int ledset = 0;
-
-  // configure leds
-  //int NUM_LEDS = board_userled_initialize();
-
-  printf("Test 1 Userleds:\n");
-  printf("All three Userleds should be on for 10 seconds.\n");
-
-  // Turn on all three leds
-  // for (int i = 0; i < NUM_LEDS; i++) {
-  //   board_userled(i, true);
-  // }
-
-  // sleep(10);
-
-  // for (int i = 0; i < NUM_LEDS; i++) {
-  //   board_userled(i, false);
-  // }
-
-  //TESTING*************
-
-  // board_userled(0, true); 
-
-
-  // sleep(3);
-
-  // board_userled(0, false);
-
-  // board_userled(1, true); 
-
-
-  // sleep(3);
-
-  // board_userled(1, false);
-
-  // board_userled(2, true); 
-  
-
-  // sleep(3);
-
-  // board_userled(2, false);
-
-  printf("Green gpio %d\n", GPIO_LED_GREEN);
-  printf("Blue gpio %d\n", GPIO_LED_BLUE);
-  printf("Red gpio %d\n", GPIO_LED_RED);
-
-  stm32_gpiowrite(GPIO_LED_GREEN, true);
-  sleep(10);
-  stm32_gpiowrite(GPIO_LED_GREEN, false);
-
-  stm32_gpiowrite(GPIO_LED_BLUE, true);
-  sleep(10);
-  stm32_gpiowrite(GPIO_LED_BLUE, false);
-
-  stm32_gpiowrite(GPIO_LED_RED, true);
-  sleep(10);
-  stm32_gpiowrite(GPIO_LED_RED, false);
-}
-
-/****************************************************************************
- * testingApp_main
- ****************************************************************************/
-int main(int argc, FAR char *argv[]) {
-  userleds_test();
-
+int main(int argc, FAR char *argv[])
+{
+  printf("Hello, I'm a Stub\n");
   return 0;
 }
