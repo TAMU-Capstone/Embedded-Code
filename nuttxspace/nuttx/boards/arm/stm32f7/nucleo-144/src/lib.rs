645b253b6df34064c2d6f3b1ee0ac7277c4a3fcc
Cole McAnelly <cole.mcanelly3@gmail.com>
2024-04-25 18:07:13 -0500

    Removed unneeded deps in Cargo.toml, fixed some BSP files, added conditional compilation to lib.rs

--- nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/lib.rs
+++ nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+// #![cfg_attr(not(test), no_std)]
 
 
 // Reference: https://os.phil-opp.com/freestanding-rust-binary/#panic-implementation
@@ -12,19 +13,58 @@ fn panic(_info: &PanicInfo) -> ! {
 mod bindings;
 mod macros;
 
-mod stm32_autoleds;
-mod stm32_reset;
-mod stm32_usb;
-mod stm32_boot;
-mod stm32_spi;
+// The following format is based off of Make.defs
+// Defaults
 mod stm32_bringup;
-mod stm32_pwm;
-mod stm32_sdio;
-mod stm32_gpio;
-mod stm32_adc;
-mod stm32_appinitialize;
-mod stm32_buttons;
+mod stm32_boot;
+
+// Conditional Files
+#[cfg(CONFIG_ARCH_LEDS)]
+mod stm32_autoleds;
+#[cfg(not(CONFIG_ARCH_LEDS))]
 mod stm32_userleds;
-// mod stm32_composite;
+
+#[cfg(CONFIG_ARCH_BUTTONS)]
+mod stm32_buttons;
+
+#[cfg(CONFIG_BOARDCTL)]
+mod stm32_appinitialize;
+
+#[cfg(CONFIG_DEV_GPIO)]
+mod stm32_gpio;
+
+#[cfg(CONFIG_SPI)]
+mod stm32_spi;
+
+#[cfg(CONFIG_ADC)]
+mod stm32_adc;
+
+#[cfg(CONFIG_PWM)]
+mod stm32_pwm;
+
+#[cfg(CONFIG_MMCSD)]
+mod stm32_sdio;
+
+#[cfg(CONFIG_STM32F7_OTGFS)]
+mod stm32_usb;
+
+// #[cfg(CONFIG_STM32F7_BBSRAM)]
+// mod stm32_bbsram;
+
+#[cfg(CONFIG_BOARDCTL_RESET)]
+mod stm32_reset;
+
+#[cfg(CONFIG_STM32_ROMFS)]
 mod stm32_romfs_initialize;
-mod stm32_dma_alloc;
+
+// #[cfg(CONFIG_SENSORS_QENCODER)]
+// mod stm32_qencoder;
+
+// #[cfg(all(CONFIG_STM32F7_CAN, CONFIG_STM32F7_CAN_CHARDRIVER))]
+// mod stm32_can;
+
+// #[cfg(all(CONFIG_STM32F7_CAN, CONFIG_STM32F7_CAN_SOCKET))]
+// mod stm32_cansock;
+
+#[cfg(CONFIG_USBDEV_COMPOSITE)]
+mod stm32_composite;
