2feb79366dfc3ede25679228b4a2c2068ebe2bcb
isidorawright <isidora.wright.1@gmail.com>
2024-04-28 18:32:19 -0500

    bbsram done and compiles

--- nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/stm32_bbsram.rs
+++ nuttxspace/nuttx/boards/arm/stm32f7/nucleo-144/src/stm32_bbsram.rs
@@ -17,215 +17,314 @@
  * under the License.
  *
  ****************************************************************************/
-use crate::bindings::*;
-use cty;
-
-#[cfg(CONFIG_STM32F7_BBSRAM)]
-{
-    /****************************************************************************
-     * Pre-processor Definitions
-     ****************************************************************************/
-    
-    /* Configuration ************************************************************/
-    
-    
-    
-    /* The following guides in the amount of the user and interrupt stack
-     * data we can save. The amount of storage left will dictate the actual
-     * number of entries of the user stack data saved. If it is too big
-     * It will be truncated by the call to stm32_bbsram_savepanic
-     */
-    
-    /* The path to the Battery Backed up SRAM */
-    
-    /* The sizes of the files to create (-1) use rest of BBSRAM memory */
-    
-    /* For Assert keep this much of the file name */
-    
-    /****************************************************************************
-     * Private Data
-     ****************************************************************************/
-    #[allow(non_camel_case_types)]
-    pub struct stack
-    {
-        sp: u32;
-        top: u32;
-        size: u32;
-    }
-    
-    #[allow(non_camel_case_types)]
-    pub struct stacks
-    {
-        user: cty::stack_t;
-        if #[cfg(CONFIG_ARCH_INTERRUPTSTACK)] > 3
+ use crate::bindings::*;
+ use core::sizeof;
+ use core::mem::zeroed;
+ use cty;
+ 
+ #[cfg(CONFIG_STM32F7_BBSRAM)]
+ {
+     /****************************************************************************
+      * Pre-processor Definitions
+      ****************************************************************************/
+     
+     /* Configuration ************************************************************/
+     
+     
+     
+     /* The following guides in the amount of the user and interrupt stack
+      * data we can save. The amount of storage left will dictate the actual
+      * number of entries of the user stack data saved. If it is too big
+      * It will be truncated by the call to stm32_bbsram_savepanic
+      */
+     
+     /* The path to the Battery Backed up SRAM */
+     
+     /* The sizes of the files to create (-1) use rest of BBSRAM memory */
+     
+     /* For Assert keep this much of the file name */
+     
+     /****************************************************************************
+      * Private Data
+      ****************************************************************************/
+     #[allow(non_camel_case_types)]
+     #[derive(Debug, Clone, Copy)]
+     pub struct stack
+     {
+         sp: u32;
+         top: u32;
+         size: u32;
+     }
+     
+     #[allow(non_camel_case_types)]
+     #[derive(Debug, Clone, Copy)]
+     pub struct stacks
+     {
+        user: stack;
+        #[cfg(CONFIG_ARCH_INTERRUPTSTACK > 3)]
         {
-            interrupt: cty::stack_t;
+            interrupt: stack;
         }
-    }
-    
-    /* Flags to identify what is in the dump */
-    enum fault_flags
-    {
+     }
+     
+     /* Flags to identify what is in the dump */
+     #[repr(u8)]
+     enum fault_flags
+     {
         const REGS_PRESENT: u32 = 0x01,
         const USERSTACK_PRESENT: u32 = 0x02,
         const INTSTACK_PRESENT: u32 = 0x04,
         const INVALID_USERSTACK_PTR: u32 = 0x20,
         const INVALID_INTSTACK_PTR: u32: = 0x40,
-    }
-    
-    #[allow(non_camel_case_types)]
-    pub struct info
-    {
-        flags: cty::fault_flags;
+     }
+     
+     #[allow(non_camel_case_types)]
+     pub struct info_t
+     {
+        flags: fault_flags;
         current_regs: usize;
         lineno: i32;
         pid: i32;
         regs: [u32; XCPTCONTEXT_REGS];
-        stacks: cty::stack_t;
-    
-        if #[cfg(CONFIG_TASK_NAME_SIZE)] > 0
+        stacks: stack;
+     
+        #[cfg(CONFIG_TASK_NAME_SIZE > 0)]
         {
             name: [char, CONFIG_TASK_NAME_SIZE + 1];
         }
-        
-        filename: [char, MAX_FILE_PATH_LENGTH];
-    }
+         
+         filename: [char, MAX_FILE_PATH_LENGTH];
+     }
+     
+     #[allow(non_camel_case_types)]
+     pub struct fullcontext
+     {
+         info: info_t;
+     
+         #[cfg(CONFIG_ARCH_INTERRUPTSTACK > 3)]
+         {
+             //stack_word_t is a u32
+             istack: [usize; CONFIG_USTACK_SIZE];
+         }
+         
+         ustack: [usize, CONFIG_ISTACK_SIZE];
+     }
+
+     type stack_word_t = usize;
+     
+     /****************************************************************************
+      * Private Data
+      ****************************************************************************/
+     
+     static gsdata: [u8, STM32F7_BBSRAM_SIZE];
+     
+     #[no_mangle]
+     pub unsafe extern "C" fn hardfault_get_desc(desc: mut* struct bbsramd_s) -> cty::c_int
+     {
+         let filestruct: struct file;
+         let ret: i32;
+     
+         ret = unsafe{file_open(&filestruct, HARDFAULT_PATH, O_RDONLY)};
+     
+         if (ret < 0)
+         {
+           unsafe
+           {
+             syslog(LOG_INFO.into(), "stm32 bbsram: Failed to open Fault Log file [%s] ""(%d)\n".as_ptr() as *const u8, HARDFAULT_PATH, ret);
+           }
+         }
+         else
+         {
+             unsafe
+             {
+                 ret = file_ioctl(&filestruct, STM32F7_BBSRAM_GETDESC_IOCTL, (unsigned long)((uintptr_t)desc));
+                 file_close(&filestruct);
+     
+                 if (ret < 0)
+                 {
+                     syslog(LOG_INFO.into(), "stm32 bbsram: Failed to get Fault Log ""descriptor (%d)\n".as_ptr() as *const u8, ret);
+                 }
+             }
+         }
+     
+         return ret;
+     }
+     
+     #[cfg(CONFIG_STM32F7_SAVE_CRASHDUMP)]
+     {
+         #[no_mangle]
+         static pub unsafe extern "C" fn copy_reverse(stack_word_t *dest, stack_word_t *src, int size)
+         {
+             while size--
+             {
+                 *dest++ = *src--;
+             }
+         }
+     }
+     
+     #[no_mangle]
+     pub unsafe extern "C" fn stm32_bbsram_int() -> cty::c_int
+     {
+         let mut filesizes: [i32, CONFIG_STM32F7_BBSRAM_FILES + 1] = BSRAM_FILE_SIZES;
+         let mut buf: [0 as char, HEADER_TIME_FMT_LEN + 1];
+         let mut desc: bbsramd_s = core::mem::zeroed();
+         let mut rv: i32;
+         let mut state: i32;
+         tt: struct tm;
+         //time_t is an i64 in rust
+     
+         /* Using Battery Backed Up SRAM */
+         unsafe{stm32_bbsraminitialize(BBSRAM_PATH.as_ptr() as *const char, filesizes.as_ptr())};
+     
+         #[cfg(CONFIG_STM32F7_SAVE_CRASHDUMP)]
+         {
+             rv = unsafe{hardfault_get_desc(&mut desc)};
+             if rv == OK
+             {
+                 unsafe
+                 {
+                     syslog(LOG_EMERG.into(), "There is a hard fault logged.\n".as_ptr() as *const u8);
+                     state = if desc.lastwrite.tv_sec != 0 || desc.lastwrite.tv_nsec != 0
+                     {
+                        0
+                     }
+                     else
+                     {
+                        1
+                     };
+ 
+                     syslog(LOG_INFO.int(), "Fault Log info File No %d Length %d flags:0x%02x ""state:%d\n".as_ptr() as *const u8, desc.fileno, desc.len, desc.flags, state);
+ 
+                     if state == OK
+                     {
+                         //time_t is an i64 in rust
+                         let time_sec: i64 = desc.lastwrite.tv_sec + (desc.lastwrite.tv_nsec / 1e9);
+                         unsafe{
+                            gmtime_r(&time_sec, &tt);
+                            strftime(buf, HEADER_TIME_FMT_LEN, HEADER_TIME_FMT, &tt);
     
-    #[allow(non_camel_case_types)]
-    pub struct fullcontext
-    {
-        info: i32;
-    
-        if #[cfg(CONFIG_ARCH_INTERRUPTSTACK)] > 3
-        {
-            //stack_word_t is a u32
-            istack: [u32; CONFIG_USTACK_SIZE];
-        }
-        
-        ustack: [u32, CONFIG_ISTACK_SIZE];
-    }
-    
-    /****************************************************************************
-     * Private Data
-     ****************************************************************************/
-    
-    static gsdata: [u8, STM32F7_BBSRAM_SIZE];
-    
-    #[no_mangle]
-    pub unsafe extern "C" fn hardfault_get_desc(desc: mut* struct bbsramd_s) -> cty::c_int
-    {
-        let filestruct: struct file;
-        let ret: i32;
-    
-        ret = unsafe{file_open(&filestruct, HARDFAULT_PATH, O_RDONLY)};
-    
-        if (ret < 0)
-        {
-          unsafe
-          {
-            syslog(LOG_INFO.into(), "stm32 bbsram: Failed to open Fault Log file [%s] ""(%d)\n".as_ptr() as *const u8, HARDFAULT_PATH, ret);
-          }
-        }
-        else
-        {
-            unsafe
-            {
-                ret = file_ioctl(&filestruct, STM32F7_BBSRAM_GETDESC_IOCTL, (unsigned long)((uintptr_t)desc));
-                file_close(&filestruct);
-    
-                if (ret < 0)
-                {
-                    syslog(LOG_INFO.into(), "stm32 bbsram: Failed to get Fault Log ""descriptor (%d)\n".as_ptr() as *const u8, ret);
+                            syslog(LOG_INFO.into(), "Fault Logged on %s - Valid\n".as_ptr() as *const u8, buf);
+                         }
+                     }
+ 
+                     rv = nx_unlink(HARDFAULT_PATH.as_ptr() as *const char);
+                     if rv < 0
+                     {
+                         unsafe {syslog(LOG_INFO.into(), "stm32 bbsram: Failed to unlink Fault Log file"" [%s] (%d)\n".as_ptr() as *const u8, HARDFAULT_PATH, rv)};
+                     }
+                 }
+             }
+         }
+ 
+         return rv;
+     }
+ 
+     #[cfg(CONFIG_STM32F7_SAVE_CRASHDUMP)]
+     {
+         pub unsafe extern "C" fn board_crashdump(sp: usize, struct tcb_s: *mut tcb, filename: *const char, lineno: i32, msg: const char, regs: *mut void)
+         {
+             pdump = &mut g_sdata as *mut fullcontext;
+             let mut rv: i32;
+ 
+             unsafe{
+                 enter_critical_section();
+ 
+                 memset(pdump, 0, sizeof(fullcontext_t));
+             }
+ 
+             lineno: (*pdump).info.lineno;
+ 
+             if filename
+             {
+                 let mut offset: i32 = 0;
+                 let filename_str = CStr::from_ptr(filename).to_str().unwrap_or("");
+                 let mut len: u32 = filename_str.len() + 1;
+ 
+                 if len > sizeof((*pdump).info.filename)
+                 {
+                     offset = len - sizeof((*pdump).info.filename);
+                 }
+ 
+                 unsafe{
+                     strlcpy(pdump.info.filename, )
+                 }
+             }
+
+             (*pdump).info.current_regs = CURRENT_REGS as usize;
+
+             #[cfg(CONFIG_TASK_NAME_SIZE) > 0]
+                unsafe{strlcpy((*pdump).info.name, (*tcb).name, sizeof(*pdump).info.name)};
+            
+             (*pdump).info.pid = (*tcb).pid;
+
+             if CURRENT_REGS
+             {
+                (*pdump).info.stacks.interrupt.sp = sp as u32;
+                (*pdump).info.flags |= fault_flags::REGS_PRESENT as u8;
+                (*pdump).info.flags |= fault_flags::USERSTACK_PRESENT as u8;
+                (*pdump).info.flags |= fault_flags::INTSTACK_PRESENT as u8;
+                unsafe{
+                    memcpy((*pdump).info.regs, CURRENT_REGS, sizeof((*pdump).info.regs));
                 }
-            }
-        }
-    
-        return ret;
-    }
-    
-    #[cfg(CONFIG_STM32F7_SAVE_CRASHDUMP)]
-    {
-        #[no_mangle]
-        static pub unsafe extern "C" fn copy_reverse(stack_word_t *dest, stack_word_t *src, int size)
-        {
-            while size--
-            {
-                *dest++ = *src--;
-            }
-        }
-    }
-    
-    #[no_mangle]
-    pub unsafe extern "C" fn stm32_bbsram_int() -> cty::c_int
-    {
-        let mut filesizes: [i32, CONFIG_STM32F7_BBSRAM_FILES + 1] = BSRAM_FILE_SIZES;
-        buf: [char, HEADER_TIME_FMT_LEN + 1];
-        desc: struct bbsramd_s;
-        rv: i32;
-        state: i32;
-        tt: struct tm;
-        //time_t is an i64 in rust
-        time_sec: i64;
-    
-        /* Using Battery Backed Up SRAM */
-        unsfae{stm32_bbsraminitialize(BBSRAM_PATH, filesizes)};
-    
-        #[cfg(CONFIG_STM32F7_SAVE_CRASHDUMP)]
-        {
-            let mut rv: i32 = unsafe{hardfault_get_desc(&desc)};
-            if rv == OK
-            {
-                unsafe
+                (*pdump).info.stacks.user.sp = (*pdump).info.regs[REG_R13];
+             }
+             else
+             {
+                (*pdump).info.flags |= fault_flags::USERSTACK_PRESENT as u8;
+                (*pdump).info.stacks.user.sp = sp as u32;
+             }
+
+             (*pdump).info.stacks.user.top = (*tcb).stack_base_ptr as usize + (*tcb).adj_stack_size as u32;
+             (*pdump).info.stacks.user.size = (*tcb).adj_stack_size as u32;
+
+             *[cfg(CONFIG_ARCH_INTERRUPTSTACK > 3)]
+             {
+                (*pdump).info.stacks.interrupt.top = g_intstacktop;
+                (*pdump).info.stacks.interrupt.size = (CONFIG_ARCH_INTERRUPTSTACK & !3) as u32;
+
+                if (*pdump).info.flags & fault_flags_t::INTSTACK_PRESENT as u8 != 0
                 {
-                    syslog(LOG_EMERG.into(), "There is a hard fault logged.\n".as_ptr() as *const u8);
-                    let mut state: i32 = (desc.lastwrite.tv_sec || desc.lastwrite.tv_nsec) ?  OK : 1;
+                    let ps: stack_word_t = (*pdump).info.stacks.interrupt.sp as *mut stack_word_t;
 
-                    syslog(LOG_INFO.int(), "Fault Log info File No %d Length %d flags:0x%02x ""state:%d\n".as_ptr() as *const u8, (unsigned int)desc.fileno, (unsigned int)desc.len, (unsigned int)desc.flags, state);
-
-                    if state == OK
-                    {
-                        //time_t is an i64 in rust
-                        let mut time_sec: i64 = desc.lastwrite.tv_sec + (desc.lastwrite.tv_nsec / 1e9);
-                        gmtime_r(&time_sec, &tt);
-                        strftime(buf, HEADER_TIME_FMT_LEN, HEADER_TIME_FMT, &tt);
-
-                        syslog(LOG_INFO.into(), "Fault Logged on %s - Valid\n".as_ptr() as *const u8, buf);
-                    }
-
-                    rv = nx_unlink(HARDFAULT_PATH);
-                    if rv < 0
-                    {
-                        syslog(LOG_INFO.into(), "stm32 bbsram: Failed to unlink Fault Log file"" [%s] (%d)\n".as_ptr() as *const u8, HARDFAULT_PATH, rv);
+                    unsafe{
+                        copy_reverse((*pdump).istack.as_mut_ptr(), &ps[nitems((*pdump).istack) / 2], nitems((*pdump).istack));
                     }
                 }
-            }
-        }
+                 if !((*pdump).info.stacks.interrupt.sp <= (*pdump).info.stacks.interrupt.top && (*pdump).info.stacks.interrupt.sp > (*pdump).info.stacks.interrupt.top - (*pdump).info.stacks.interrupt.size)
+                 {
+                    (*pdump).info.flags != fault_flags::INVALID_INTSTACK_PTR as u8;
+                 }
+             }
+             if (*pdump).info.flags & fault_flags::USERSTACK_PRESENT as u8 != 0
+             {
+                let ps = (*pdump).info.stacks.user.sp as *mut stack_word_t;
+                unsafe{
+                    copy_reverse((*pdump).ustack, &ps[nitems((*pdump).ustack) / 2], nitems((*pdump).ustack));
+                }
+             }
 
-        return rv;
-    }
+             if !((*pdump).info.stacks.user.sp <= (*pdump).info.stacks.user.top && (*pdump).info.stacks.user.sp > (*pdump).info.stacks.user.top - (*pdump).info.stacks.user.size)
+             {
+                 (*pdump).info.flags |= fault_flags_t::INVALID_USERSTACK_PTR as u8;
+             }
 
-    #[cfg(CONFIG_STM32F7_SAVE_CRASHDUMP)]
-    {
-        pub unsafe extern "C" fn board_crashdump(uintptr_t sp, struct tcb_s *tcb, const char *filename, int lineno, const char *msg, void *regs)
-        {
-            pdump: *mut fullcontext = &mut g_sdata as *mut fullcontext;
-            rv: i32;
+             let rv = stm32_bbsram_savepanic(HARDFAULT_FILENO as i32, pdump as *mut u8, sizeof(fullcontext));
 
-            unsafe{
-                enter_critical_section();
-
-                memset(pdump, 0, sizeof(fullcontext_t));
-            }
-
-            lineno: info.lineno;
-
-            if filename
-            {
-                offset: i32 = 0;
-                len: u32 = (filename + 1).len();
-
-                if len > 
-            }
-        }
-    }
-}
+             if rv == -EXNIO
+             {
+                let dead = b"Memory wiped - dump not saved!\0";
+                while *dead
+                {
+                    unsafe{
+                        arm_lowputc(*dead++);
+                    }
+                }
+             }
+             else if rv == -ENOSPC
+             {
+                arm_lowputc('!');
+             }
+         }
+     }
+ }
+ 
